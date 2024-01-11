be29c799c10480d329374a0e36497253b5cb7344
doldamul <junha0504@gmail.com>
2024-01-11 16:49:48 +0900

    Add toolbar, modify add button to overlayed view

--- ContentView.swift
+++ ContentView.swift
@@ -4,42 +4,44 @@ struct ContentView: View {
     @State var journals: [Journal] = []
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Journal")
-                    .fontWeight(.bold)
-                Spacer()
-                Image(systemName: "calendar")
-                    .foregroundStyle(.secondary)
-                    .symbolRenderingMode(.multicolor)
-            }
-            .font(.largeTitle)
-            .padding()
-            
-            NavigationStack {
-                List(journals) { journal in
-                    NavigationLink {
-                        Text("Detail View")
-                    } label: {
-                        let isEmpty = journal.title.isEmpty
-                        Text(isEmpty ?"New Journal": journal.title)
-                            .opacity(isEmpty ? 0.5 : 1)
-                    }
+        NavigationStack {
+            List(journals) { journal in
+                NavigationLink {
+                    Text("Detail View")
+                } label: {
+                    let isEmpty = journal.title.isEmpty
+                    Text(isEmpty ? "New Journal" : journal.title)
+                        .opacity(isEmpty ? 0.5 : 1)
                 }
             }
-            
-            Button(action: addJournal) {
-                Image(systemName: "plus")
-                    .foregroundStyle(Color.accentColor)
-                    .imageScale(.large)
-                    .bold()
-                    .padding()
+            .navigationTitle("Journal")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Journal")
+                        .font(.headline)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "calendar")
+                        .foregroundStyle(.secondary)
+                        .symbolRenderingMode(.multicolor)
+                }
             }
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.circle)
-            .tint(.white)
-            .shadow(radius: 10)
-            .padding()
+            .overlay(alignment: .bottom) {
+                Button(action: addJournal) {
+                    Image(systemName: "plus")
+                        .foregroundStyle(Color.accentColor)
+                        .imageScale(.large)
+                        .bold()
+                        .padding()
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.circle)
+                .tint(.white)
+                .shadow(radius: 10)
+                .padding()
+            }
         }
     }
     
